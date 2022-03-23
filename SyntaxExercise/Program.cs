956f298b7288f6063417efea6cf81f4d205cb30e
mofulbright <mofulbright02@gmail.com>
2022-03-23 15:43:35 -0300

    initial

---
+++ SyntaxExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SyntaxExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+            var answer = 4;
+            var response = (answer < 9) ? "is less than 9" : "is greater than or equal to 9";
+            Console.WriteLine($"{answer} {response}.");
+
+
+        }
+    }
+}
